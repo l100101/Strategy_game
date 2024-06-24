9c390d6cf7f67e252ff8fb59b5f6fde4361c58ee
l100101 <ltlrg779@gmail.com>
2024-06-24 20:04:22 +0400

    nahuevertil platformio

--- Strategy_game/alldefs.h
+++
@@ -1,5 +0,0 @@
-#include "GBUSmini.h" // мини-библиотека с лёгкими функциями
-#include "tower.h"
-#include "gameField.h"
-#include "unit.h"
-#include <iostream>
---
+++ alldefs.h
@@ -0,0 +1,5 @@
+#include "../libs/GyverBus/GBUSmini.h" // мини-библиотека с лёгкими функциями
+#include "../libs/tower/tower.h"
+#include "../libs/gameField/gameField.h"
+#include "../libs/unit/unit.h"
+#include <iostream>
